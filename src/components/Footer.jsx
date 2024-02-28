ec358fe9d4d239e30c2dc06a03dcbb4ee214ead2
Jon <jyoung57@W-BC91R73>
2024-02-27 20:26:39 -0600

    add scaffold

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; Codex Academy</p>
+    </footer>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../styles/Main.css"
+
+export default function Main() {
+  return (
+    <main>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit,
+      ut. Ipsa, illum quod alias numquam tempore ex? Placeat optio harum
+      molestiae dolor exercitationem quaerat aut, dignissimos iure enim, nihil
+      expedita? Quis, quisquam eaque! Tempora quod beatae autem saepe
+      reiciendis, harum molestiae id enim ex, debitis nihil voluptates
+      distinctio sed repudiandae quis quaerat dolore! Minima id non totam
+      reiciendis, facilis corrupti.
+    </main>
+  );
+}

7322d94b54ba50721cb12a7d1a4bc68d2898051b
Jon <jyoung57@W-BC91R73>
2024-03-06 20:43:07 -0600

    added dark mode setting, but still testing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import "../styles/Header.css";
 
 export default function Header() {
+
+  // function App() {
+  // const [darkMode, setDarkMode] = useState(false);
+
+  // const toggleDarkMode = () => {
+  //   setDarkMode(prevMode => !prevMode);
+  // };
   return (
+<>
     <header>
       <h1>Daily Org Chart</h1>
+      <h2>Helping you stay on track, day by day!</h2>
     </header>
-  )
-}
+
+    {/* <div className={darkMode ? 'dark-mode' : 'light-mode'}>
+      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
+      <h1>Hello, World!</h1>
+      <p>This is a sample text.</p>
+    </div> */}
+
+</>
+   );
+  }
+// }

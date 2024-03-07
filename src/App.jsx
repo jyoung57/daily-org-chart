9fb98b81234ba66e712a107bc2d4e69d8e8ff61b
Jon <jyoung57@W-BC91R73>
2024-03-06 21:11:01 -0600

    added date/time stamp functionality, but testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
+
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
+
+  const [taskData, setTaskData] = useState ([])
+  
+  function Task(date){
+    this.date = date;
+    // Will use this to create task description
+    this.type = "";
+  }
+
   return (
     <>
       <Header />
 
-      <Main />
+      <Main taskData={taskData}/>
 
       <Footer />
     </>
